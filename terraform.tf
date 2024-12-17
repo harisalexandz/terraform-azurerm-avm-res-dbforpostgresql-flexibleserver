9fa1095843b94734230825f96e077e836e3e1133
Zacharias Alexandridis <zacharias.alexandridis@trasys.gr>
2024-12-17 19:53:34 +0200

    Change required version

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.9"
+  required_version = "~> 1.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.12"
+      version = "~> 3.105"
     }
     modtm = {
       source  = "Azure/modtm"
